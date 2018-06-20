c1801ac7123a856f890c58c512acddf47755cbd4
David Lyon <david.lyon@clixx.io>
2018-06-21 03:46:44 +1000

    Added Exception handler to allow to scrape new Coins

--- src/plutonscrape.py
+++ src/plutonscrape.py
@@ -34,7 +34,7 @@ apph5datafile = os.path.join(appdatadir,apph5data)
 config = configparser.ConfigParser(allow_no_value=True)
 config.read(appfullconfig)
 
-def updateCoins(coinList):
+def updateCoins(coinCodes):
     """Update the H5 Repository with Coin Data
 
     Keyword arguments:
@@ -43,40 +43,49 @@ def updateCoins(coinList):
 
     global apph5datafile
     
-    print("You asked to scrape coins %s" % (coinList))
+    coinList = []
     
-    store = HDFStore(apph5datafile)
-    storedcoins = store.keys()
-    print "Keys are:", storedcoins
-    store = None
-    
+    if coinCodes == '*':
+        print("You asked to update all tracked coins")
+        coinList = config.get('Active','Coins').split(' ')
+    else:
+        coinList = coinCodes
+        print("You asked to list the last week for coins %s" % (coinList))
+   
+    print("You asked to update coins %s" % (coinList))
+
     for coin in coinList:
 
         havecointable = True
         
         if not os.path.exists(apph5datafile):
-			print("Creating Datafile %s" % apph5datafile)
-			havecointable = False
-			
-        if not ('/'+coin.upper()) in storedcoins:
-            print("Coin %s not in dataset" % coin.upper())
+            print("Creating Datafile %s" % apph5datafile)
             havecointable = False
-	
+
+#        if not ('/'+coin.upper()) in storedcoins:
+#            print("Coin %s not in dataset" % coin.upper())
+#            havecointable = False
+
         if havecointable:
-			
-            hdf = read_hdf(apph5datafile,coin)
 
-            hdf['date_ranked']=hdf['Date'].rank(ascending=1)
-            print "Last element from %s is dated %s" % (coin,str(hdf.at[0,'Date'])[:10])
-            lastdate = datetime.strptime(str(hdf.at[0,'Date'])[:10],"%Y-%m-%d")
-            nextday = timedelta(1)
-            nextday = lastdate + nextday
-            delta = (datetime.today() - timedelta(1)) - nextday
+            try:
+                hdf = read_hdf(apph5datafile,coin)
+
+                hdf['date_ranked']=hdf['Date'].rank(ascending=1)
+                print "Last element from %s is dated %s" % (coin,str(hdf.at[0,'Date'])[:10])
+                lastdate = datetime.strptime(str(hdf.at[0,'Date'])[:10],"%Y-%m-%d")
+                nextday = timedelta(1)
+                nextday = lastdate + nextday
+                delta = (datetime.today() - timedelta(1)) - nextday
+                print("Difference from yesterday to last update ", delta)
+
+            except KeyError:
+                print("No data found for this Coin")
+                nextday = (datetime.today() - timedelta(2*365))
+
             lastdate = datetime.today() - timedelta(1)
-            print "Difference from yesterday to last update ", delta
-            print "We need to request from %s to %s" % (str(nextday),str(lastdate))
 
-            nextday = (datetime.today() - timedelta(2*365))
+            print "We need to request from %s to %s" % (str(nextday),str(lastdate))
 
             startdate = '{:%d-%m-%Y}'.format(nextday)
             enddate = '{:%d-%m-%Y}'.format(lastdate)
@@ -84,21 +93,26 @@ def updateCoins(coinList):
             print "Requesting data from", startdate, enddate
 
             scraper = CmcScraper(coin,startdate,enddate)
-            
+
         else:
-			
+
             scraper = CmcScraper(coin.upper())
 
         # get data as list of list
-        headers, data = scraper.get_data()
+        try:
+            headers, data = scraper.get_data()
 
-        # get dataframe for the data
-        df = scraper.get_dataframe()
+            # get dataframe for the data
+            df = scraper.get_dataframe()
 
-        print df
+            print df
 
-        # df.to_hdf('pluton.h5',coin,append=True,format='t')
-        df.to_hdf(apph5datafile,coin.upper())
+            # df.to_hdf('pluton.h5',coin,append=True,format='t')
+            df.to_hdf(apph5datafile,coin.upper())
+
+        except ValueError:
+            print("No data found")
+
 
     return
     
