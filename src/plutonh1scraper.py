0f13042a65940a54c4cc7d9174d61db47e9cebca
David Lyon <david.lyon@clixx.io>
2018-09-02 10:51:14 +1000

    Add JSON Output including Time Data

--- src/plutonh1scraper.py
+++ src/plutonh1scraper.py
@@ -1,16 +1,13 @@
 import sys, getopt
 import time, urllib2
-import argparse, configparser
+import argparse, configparser, json, locale, re
 from bs4 import BeautifulSoup
 import paho.mqtt.publish as publish
 
 # specify the url
 quote_page = "https://www.coinspot.com.au/buy/"
 
-# specify the mqtt host
-mqtt_host = "192.168.1.105"
-
-def scrapeCoin(coincode):
+def scrapeCoin(coincode,broker):
 	
     try:
 		
@@ -26,48 +23,53 @@ def scrapeCoin(coincode):
         # Take out the <div> of name and get its value
         name_box = soup.find('h1', attrs={'class': 'price-title'})
 
-        # strip() is used to remove starting and trailing
-        price = name_box.text.strip() 
-        print price
+        # Clean up the price, removing currency codes and spaces
+        decimal_point_char = locale.localeconv()['decimal_point']
+        clean = re.sub(r'[^0-9'+decimal_point_char+r']+', '', str(name_box.text.strip()))
+        price = float(clean)
         
-        if len(mqtt_host):
-            publish.single("pluton/test", price, hostname=mqtt_host)
+        print coincode + ":" + str(price)
+        
+        if len(broker):
+            data = { coincode: price, 'ts' : time.time() }
+            publish.single("pluton/price", json.dumps(data), hostname=broker)
 
     except urllib2.URLError:
         time.sleep(int(percoin_interval))
 
 if __name__ == "__main__":
-	
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("coin", metavar='Coin', nargs='*',help="Coin Code",default='*')
-    parser.add_argument("--host", help="MQTT Broker Address")
-	
+    parser.add_argument("coin", metavar='Coin', nargs='*',default='*', help="Coin Code")
+    parser.add_argument("--broker", default='127.0.0.1', help="MQTT Broker Address")
+
     args = parser.parse_args()
-	
+
     coinList = getattr(args, 'coin')
 
-    if len(args.host):
-        mqtt_host = mqtt_host
-  	
+    if args.broker:
+        mqtt_host = args.broker
+        print 'Publishing Price Data to MQTT Broker ', args.broker
+        
+    exit    
+
     # query the website and return the html to the variable page
     hdr = {'User-Agent': 'Mozilla/5.0'}
-    
-    # coinList = ['DGB',]
-    
+
     percoin_interval = 30
-    
+
     print("You asked to monitor coins %s" % (coinList))
 
     while True:
-		
+
         try:
 
             for coin in coinList:
 
-                scrapeCoin(coin)
-	
+                scrapeCoin(coin,args.broker)
+
                 time.sleep(int(percoin_interval))
 
         except Exception as e: 
             print(e)
-            
+
